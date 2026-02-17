eace3fcbb55b12f576e6ad80b1806f0ee15613b9
Fauzan Rhamzy <fauzan.rhamzy01@gmail.com>
2026-02-17 18:24:09 +0700

    changed some file structure and changed the logo configuration

--- client/src/components/login-form.tsx
+++ client/src/components/login-form.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import GoogleIcon from "@/assets/GoogleIcon";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
@@ -17,7 +17,7 @@ export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
